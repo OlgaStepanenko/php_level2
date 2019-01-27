f1bf7e1ff397d612781da3617885593712f6a2b8
OlgaStepanenko <olea13lea@gmail.com>
2019-01-27 23:58:18 +0300

    refactoring

--- class/class.products.php
+++ class/class.products.php
@@ -13,10 +13,11 @@ class products {
     public $name = "карандаш";
     public $price = "45";
 
-    function set ($category, $name, $price) {
+    function __construct($category, $name, $price) {
         $this->category = $category;
         $this->name = $name;
         $this->price = $price;
+        $this->getInfoProduct();
     }
 
     function getInfoProduct () {
@@ -24,11 +25,11 @@ class products {
     }
 }
 
-$pencil = new products; 
-$pencil->set(" Канцелярские товары ", " Карандаш ", " 30 ");
-$pencil->getInfoProduct();
+new products(" Канцелярские товары ", " Карандаш ", " 30 "); 
 
-
+class pencilsKindOf extends products {
+    
+}
 ?>
 </body>
 </html>
