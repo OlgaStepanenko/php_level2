adf10bdafbd03d009d417258eba4e4789300b41e
OlgaStepanenko <olea13lea@gmail.com>
2019-01-29 00:22:10 +0300

    create zadanie 5

--- class/class.colourPencils.php
+++ class/class.colourPencils.php
@@ -1,20 +1,23 @@
 <?php
-include "class.products.php";
+include './class.products.php';
 
 class colourPencils extends products {
-    public $numberOfColors = " 0 ";
+    public $name = " Цветные карандаши ";
+    public $numberOfColors = " 18 ";
+
 
     function __consrtuct($category, $name, $price, $numberOfColors) {
-        $this->category = $category;
+        perent::__consrtuct($category, $price);
+        //$this->category = $category;
         $this->name = $name;
-        $this->price = $price;
+        //$this->price = $price;
         $this->numberOfColors = $numberOfColors;
     }
 
-    function getInfoProduct() {
-        echo $this->name."количество цветов в упаковке".$this->numberOfColors;
+    function getInfoColorPencils() {
+        echo $this->name.", количество цветов в упаковке".$this->numberOfColors;
     }
 }
 
-$object = new colourPencils(" Канцелярские товары ", " Карандаши ", 40, 32);
-$object->getInfoProduct();
+$object = new colourPencils(" Канцелярские товары ", " Цветные карандаши", 40, 32);
+$object->getInfoColorPencils();
